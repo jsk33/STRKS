219ba6692634f69da93e39c22f921111eb160ac4
jsk33 <jason33kim@gmail.com>
2020-03-24 14:39:22 -0700

    due date included in Target model

--- models/Target.js
+++ models/Target.js
@@ -20,6 +20,11 @@ const TargetSchema = mongoose.Schema(
             type: Boolean,
             required: true,
             default: false
+        },
+        due: {
+            type: Date,
+            required: true,
+            default: new Date(new Date().setHours(24, 0, 0, 0))
         }
     }
 ); 
